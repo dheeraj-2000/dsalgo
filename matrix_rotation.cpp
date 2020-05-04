fbbffc562948efd510fd886df5f8c1428b1cb962
dheeraj2000 <dheeraj12000@gmail.com>
2020-05-04 21:22:29 +0530

    updated mat rotat

--- matrix_rotation.cpp
+++ matrix_rotation.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int transpos_matrix(int matrix[N][N]){
       for (int i=0; i<N; i++){
-            for (int j=0; j<N; j++){
+            for (int j=i; j<N; j++){
                   swap(matrix[i][j], matrix[j][i]);
             }
       }
@@ -14,7 +14,7 @@ int transpos_matrix(int matrix[N][N]){
 
 int reverse_rows(int matrix[N][N]){
       for (int i=0; i<N; i++){
-            for (int j=i; j<N-1; j++){
+            for (int j=0; j<N-1; j++){
                   swap(matrix[i][j], matrix[i][j]);
             }
       }
@@ -24,17 +24,23 @@ int reverse_rows(int matrix[N][N]){
 int print_matrix(int matrix[N][N]){
       for (int i=0; i<N; i++){
             for (int j=0; j<N; j++){
-                  cout<< '\t' << matrix[i][j];
+                  cout << matrix[i][j];
             }
+            cout << endl;
       }
-      cout << "\n";
+      cout <<endl;
 }
 
 // Driver function to test above function
 int main(){
       int matrix[N][N] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-      cout<< "The given matrix is: " << print_matrix[N][N];
-      transpos_matrix[N][N];
-      cout<< "Transpose matrix of the given matrix is: " << print_matrix[N][N];
-      reverse_rows[N][N];
-      cout<< "Rotated matrix is: " << print_matrix[N][N];
+      cout<< "The given matrix is: " << endl;
+      print_matrix( matrix);
+      transpos_matrix( matrix);
+      // cout<< "Transpose matrix of the given matrix is: " << endl;
+
+      cout<< "Rotated matrix is: " << "\n" << print_matrix( matrix);
+      reverse_rows( matrix);
+      
+
+}
