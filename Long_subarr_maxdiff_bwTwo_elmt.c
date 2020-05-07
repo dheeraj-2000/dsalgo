c24a7d70e6a8769ba20f86e7dc88e5c70540a37e
dheeraj2000 <dheeraj12000@gmail.com>
2020-05-07 23:43:39 +0530

    updated Longest subarray with maximum diff of x b/w two elements

---
+++ Long_subarr_maxdiff_bwTwo_elmt.c
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+void longest_subarray(int *arr, int n, int diff){
+
+
+
+
+}
+
+
+
+
+
+
+
+
+// Driver code
+int main(){
+      int n, diff;
+      cin>>n;
+      int arr[n];
+      for (int i=0; i<n; i++){
+            cin>>arr[i]
+      }
+      cout >> "enter the differenc between two elements of the subarray: ";
+      cin>> diff;
+      longest_subarray(arr, n, diff);
+}
