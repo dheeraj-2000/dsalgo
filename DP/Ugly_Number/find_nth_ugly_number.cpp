a21c3a741e07345d21204799f9cf6f1c7068928b
dheeraj2000 <dheeraj12000@gmail.com>
2020-05-26 23:15:22 +0530

    added impt comments nth ugly umber

--- DP/Ugly_Number/find_nth_ugly_number.cpp
+++ DP/Ugly_Number/find_nth_ugly_number.cpp
@@ -18,29 +18,29 @@ int get_min(int x, int y, int z){           //to find minimum of three number (y
       }
    }
 
-int get_nth_ugly_number(int n){                  
-      int i_2=0,  i_3=0, i_5=0;
-      int ugly[n];
-      ugly[0]=1;
-      long long int track_next_ugly = 1;
+int get_nth_ugly_number(int n){           //function to find nth ugly number
+      int iterattor_factor_2=0,  iterattor_factor_3=0, iterattor_factor_5=0;
+      int ugly[n];                         //arrar to store ugly numbers
+      ugly[0]=1;                           //defining initial uglgy number which is 1
+      long long int track_next_ugly = 1;    //to track next coming ugly number, hence defining st to be 1
       long long int next_multiple_of2 = 2;
       long long int next_multiple_of3 = 3;
       long long int next_multiple_of5 = 5;
 
       for(int i=1; i<n; i++){
-            track_next_ugly = get_min(next_multiple_of2, next_multiple_of3, next_multiple_of5);
+            track_next_ugly = get_min(next_multiple_of2, next_multiple_of3, next_multiple_of5);     //finding next ugly number
             ugly[i]= track_next_ugly;
             if(track_next_ugly==next_multiple_of2){
-                  i_2++;
-                  next_multiple_of2 = ugly[i_2]*2;
+                  iterattor_factor_2++;                                              //increase iterator of ugly numbers whose factor is 2
+                  next_multiple_of2 = ugly[iterattor_factor_2]*2;
             }
             if(track_next_ugly==next_multiple_of3){
-                  i_3++;
-                  next_multiple_of3 = ugly[i_3]*3;
+                  iterattor_factor_3++;                                              //increase iterator of ugly numbers whose factor is 3
+                  next_multiple_of3 = ugly[iterattor_factor_3]*3;
             }
             if(track_next_ugly==next_multiple_of5){
-                  i_5++;
-                  next_multiple_of5 = ugly[i_5]*5;
+                  iterattor_factor_5++;                                             //increase iterator of ugly numbers whose factor is 5
+                  next_multiple_of5 = ugly[iterattor_factor_5]*5;
             }
       }
       return track_next_ugly;
