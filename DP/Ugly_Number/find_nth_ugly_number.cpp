c0a30de96bc638fca17850b1b72a56b2060e4a31
dheeraj2000 <dheeraj12000@gmail.com>
2020-05-26 22:49:19 +0530

    added min nd driver function for finding nth ugly umber

--- DP/Ugly_Number/find_nth_ugly_number.cpp
+++ DP/Ugly_Number/find_nth_ugly_number.cpp
@@ -1,2 +1,31 @@
 // Ugly numbers are those numbers which are disvible by 2,3,5 (by default with 1).
 // @dheeraj
+
+#include<iostream>
+using namespace std;
+
+int get_min(int x, int y, int z){           //to find minimum of three number (you cna also use built-in function for min)
+      if(x < y) {
+         if(x < z)
+            return x;
+         else
+            return z;
+      }else {
+         if(y < z)
+            return y;
+         else
+            return z;
+      }
+   }
+
+
+
+
+
+// Driver function
+int main(){
+      int n;
+      cout<< "Which nth ugly number you wanna find??: ";
+      cin >> n;
+      cout<< n <<"th ugly number is: "<<get_nth_ugly_number(n);
+}
