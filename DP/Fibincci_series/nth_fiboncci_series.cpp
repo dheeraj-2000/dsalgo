95e35eae4eb7cb3e150da7c128df86f4eb825d0d
dheeraj2000 <dheeraj12000@gmail.com>
2020-05-27 20:28:43 +0530

    updated fibonacci code

--- DP/Fibincci_series/nth_fiboncci_series.cpp
+++ DP/Fibincci_series/nth_fiboncci_series.cpp
@@ -1,17 +1,26 @@
 // Fibonacci numbers are the numbers in the following integer sequence. 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ……..
 // @dheeraj
 
-#include<iostrem>
+#include<iostream>
 using namespace std;
 
-
-
+int getnth_fibonaacci(int n){
+      // int n1=0, n2=1;
+      int fibo[n+2];
+      int i;
+      fibo[0]=0;                 // Declraing 0th term and 1th term to be 1.
+      fibo[1]=1;
+      for (i=2; i<=n; i++){
+            fibo[i] = fibo[i-1] + fibo[i-2];                //Adding previous two terms and storing it
+      }
+      return fibo[n];
+}
 
 // Driver Code
 int main() {
       /* code */
       int n;
-      cout <<"Enter the nth term: ";
+      cout<<"Enter the nth term: ";
       cin>>n;
       cout<<n<<"th Fibonacci number is: "<<getnth_fibonaacci(n);
       return 0;
