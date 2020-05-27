dfb4671b47f0e139853bff05444c9c605ff85e0b
dheeraj2000 <dheeraj12000@gmail.com>
2020-05-27 21:08:22 +0530

    added alterntive approach of reduced complexity using formula

---
+++ DP/Fibincci_series/alterntive_formula_method.cpp
@@ -0,0 +1,20 @@
+// Fibonacci numbers are the numbers in the following integer sequence. 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ……..
+// @dheeraj
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int getnth_fibonaacci(int n) {
+  double phi = (1 + sqrt(5)) / 2;
+  return round(pow(phi, n) / sqrt(5));
+}
+
+// Driver Code
+int main() {
+      /* code */
+      long int n;
+      cout<<"Enter the nth term: ";
+      cin>>n;
+      cout<<n<<"th Fibonacci number is: "<<getnth_fibonaacci(n);
+      return 0;
+}
