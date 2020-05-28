2a64f7d98f069867e1606b1024b37e2d5c96802e
dheeraj2000 <dheeraj12000@gmail.com>
2020-05-28 19:07:41 +0530

    creted sum of n Fibbonacci sequence

---
+++ DP/Fibincci_series/sum_of_nFibonacci_seq.cpp
@@ -0,0 +1,3 @@
+// Fibonacci numbers are the numbers in the following integer sequence. 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ……..
+// Key: Sum of 'n' Fibonacci sequence will be [](n+2)th term -1] 
+// @dheeraj
