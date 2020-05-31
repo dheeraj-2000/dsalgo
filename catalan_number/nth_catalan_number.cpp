43e7dcce16e4098ccabc10cf66c3b09b0f007f4f
dheeraj2000 <dheeraj12000@gmail.com>
2020-05-31 14:10:21 +0530

    updated catalan prob

--- catalan_number/nth_catalan_number.cpp
+++ catalan_number/nth_catalan_number.cpp
@@ -1,1 +1,20 @@
 // Catalan numbers form a sequence of natural numbers that occur in various counting problems
+// examples are : 1 1 2 5 14 42 132 429 1430 4862
+// @dheeraj
+
+#inlcude<bits/stdc++.h>
+using namespace std;
+
+int get_nth_catalan_number(int n){
+
+}
+
+
+// Driver function
+int main (){
+      int n;
+      cout<<"Enter the nth number: ";
+      cin>>n;
+      cout<<n<<"th Catalan number will be: "<<get_nth_catalan_number(n);
+
+}
