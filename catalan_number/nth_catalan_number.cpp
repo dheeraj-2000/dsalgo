a39ff5c464bfad8d9deacceaf0f01da4a3a890f6
dheeraj2000 <dheeraj12000@gmail.com>
2020-06-01 20:20:14 +0530

    updated catalan

--- catalan_number/nth_catalan_number.cpp
+++ catalan_number/nth_catalan_number.cpp
@@ -10,7 +10,7 @@ unsigned long int get_nth_catalan_number(unsigned int n){
       unsigned long int cat[n+1];
       cat[0]=1;
       cat[1]=1;
-      for (int i=2; i<n; i++){
+      for (int i=2; i<=n; i++){
             cat[i]=0;
             for (int j=0; j<i; j++){
                   cat[i]+= cat[j]*cat[i-j-1];
