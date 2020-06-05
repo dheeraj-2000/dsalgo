5c8f7a21788bff7e9f6ff17efadc16b55222d3f0
dheeraj2000 <dheeraj12000@gmail.com>
2020-06-05 23:46:16 +0530

    added permutaion coefficient

---
+++ DP/Permutation Coefficient/permutation_coefficient.cpp
@@ -0,0 +1,5 @@
+// The Permutation Coefficient represented by P(n, k) is used to represent the number of ways to obtain an ordered subset having k elements
+// from a set of n elements.
+// P(10, 2) = 90
+// P(10, 3) = 720
+//@dheeraj
