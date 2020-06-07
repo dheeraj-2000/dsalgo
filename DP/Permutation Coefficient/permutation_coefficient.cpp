d6efd3320d8a17ffcebe798c45fe77bf3b85eae9
dheeraj2000 <dheeraj12000@gmail.com>
2020-06-07 20:03:48 +0530

    updated permutaion coefficient

--- DP/Permutation Coefficient/permutation_coefficient.cpp
+++ DP/Permutation Coefficient/permutation_coefficient.cpp
@@ -3,3 +3,23 @@
 // P(10, 2) = 90
 // P(10, 3) = 720
 //@dheeraj
+#include<bits/stdc++.h>
+using namespace std;
+
+int getPermutaionCoeff(int n, int k){
+      int fibo[n+1];
+      fibo[0]=1;
+      for (int i=1; i<=n; i++){
+            fibo[i] = i*fibo[i-1];
+      }
+      return fibo[n]/ fibo[n-k];
+}
+
+
+// Driver function
+int main(){
+      int n, k;
+      cout<<"Enter the value of n and k: ";
+      cin>>n>>k;
+      cout<<"Permutation Coefficient of n and k is: "<<getPermutaionCoeff(n, k);
+}
