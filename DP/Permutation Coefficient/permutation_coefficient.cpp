2c291c799426f7e41d9ac839af66b02a5ebf9d07
dheeraj2000 <dheeraj12000@gmail.com>
2020-06-07 20:06:07 +0530

    added comments in the code

--- DP/Permutation Coefficient/permutation_coefficient.cpp
+++ DP/Permutation Coefficient/permutation_coefficient.cpp
@@ -8,11 +8,11 @@ using namespace std;
 
 int getPermutaionCoeff(int n, int k){
       int fibo[n+1];
-      fibo[0]=1;
+      fibo[0]=1;                                                                    // Taking the first vakue of array as 1 i.e, fatorial of 0 is 1
       for (int i=1; i<=n; i++){
             fibo[i] = i*fibo[i-1];
       }
-      return fibo[n]/ fibo[n-k];
+      return fibo[n]/ fibo[n-k];                                                    //return the value of P(n, k) using the formula method
 }
 
 
@@ -20,6 +20,6 @@ int getPermutaionCoeff(int n, int k){
 int main(){
       int n, k;
       cout<<"Enter the value of n and k: ";
-      cin>>n>>k;
+      cin>>n>>k;                                                                          // Taking the value of n and k
       cout<<"Permutation Coefficient of n and k is: "<<getPermutaionCoeff(n, k);
 }
