8fc417a222ed3558338c0a7f2cfb0135b761d7b7
dheeraj2000 <dheeraj12000@gmail.com>
2020-06-30 22:02:33 +0530

    added new code

---
+++ celcius_to_farenheit.py
@@ -0,0 +1,12 @@
+print("press(1) for celsius to fahrenheit")
+print("press(2) for fahrenheit to celsius")
+print("enter the choise:")
+choise=int(input())
+if(choise=1):
+celsius=print(input())
+fahrenheit=(celsius*1.8+32)
+print("your answer is",fahrenheit)
+if(choise=2):
+fahrenheit=print(input())
+celsius=((fahrenheit-32)\1.8)
+print("your answer is",celsius)
