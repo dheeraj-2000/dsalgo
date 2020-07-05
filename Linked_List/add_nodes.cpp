000cf524e8bf2e0becc7a83e84eee03d313a6474
dheeraj2000 <dheeraj12000@gmail.com>
2020-07-05 20:29:15 +0530

    updated

--- Linked_List/add_nodes.cpp
+++ Linked_List/add_nodes.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Node {                   //defining a node
+class Node {                                                   //defining a node
 public:
   int data;
   Node* next;
