fd0247153933deee9cbabe6177cf796c513abf5d
dheeraj2000 <dheeraj12000@gmail.com>
2020-08-23 22:23:54 +0530

    updated postman quetsion

---
+++ large_response_postman.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+int main()
+{
+    char x;
+    ifstream f("host.txt",ios::ate);
+    streampos size = f.tellg();
+    for(int var=1;var<=size;var++){
+        f.seekg(-var,ios::end);
+        f.get(x);
+        printf("%c",x);
+    }
+    return 0;
+}
