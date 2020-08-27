f5364f1422842152c729c760a94b39c1e7d0a025
dheeraj2000 <dheeraj12000@gmail.com>
2020-08-27 18:29:07 +0530

    added reverse 32 bit num

--- Leetcode problems/reverse_integer.cpp
+++ Leetcode problems/reverse_integer.cpp
@@ -5,8 +5,8 @@ public:
       int reverse_num(int t){
             int result = 0;
             while(t !=0){
-                  if (INT_MAX < t) return 0;
-                  if (INT_MIN > t) return 0;
+                  if (INT_MAX > t) return 0;
+                  if (INT_MIN < t) return 0;
 
                   result = result*10 + (result%10);
                   t = t/10;
