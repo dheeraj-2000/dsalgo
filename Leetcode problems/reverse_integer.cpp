791ad0895193735440d7c7e24b11694ac780e3ab
dheeraj2000 <dheeraj12000@gmail.com>
2020-08-27 18:36:46 +0530

    correction

--- Leetcode problems/reverse_integer.cpp
+++ Leetcode problems/reverse_integer.cpp
@@ -5,18 +5,18 @@ public:
       int reverse_num(int t){
             int result = 0;
             while(t !=0){
-                  if (INT_MAX > t) return 0;
-                  if (INT_MIN < t) return 0;
+                  if (INT_MAX/10 < result) return 0;
+                  if (INT_MIN/10 > result ) return 0;
 
-                  result = result*10 + (result%10);
+                  result = result*10 + (t%10);
                   t = t/10;
             }
-            return t;
+            return result;
       }
 };
 
 int main(){
-      int num = 123;
+      int num = -123;
       Solution ob;
       cout<<ob.reverse_num(num);
 }
