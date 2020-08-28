142c8b2ab38ed2cc7606b5407353eeddb0208d72
dheeraj2000 <dheeraj12000@gmail.com>
2020-08-28 12:19:49 +0530

    updated

---
+++ Leetcode problems/pallindrome.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+public:
+      bool check_pallindrome(int num){
+            int reverse_num = 0;
+            int temp = num, flag = 0;
+            if (num <0 )
+                  return false;
+            else{
+                  while(temp !=0){
+                        reverse_num = reverse_num*10 + temp%10;
+                        temp = temp/10;
+                  }
+            }
+            return reverse_num == num;
+      }
+};
+
+int main(){
+      int num = 0;
+      Solution object;
+      cout<<object.check_pallindrome(num);
+}
