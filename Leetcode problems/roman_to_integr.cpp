e200dafd833327aa6d421f35a33ab18aa79cfc44
dheeraj2000 <dheeraj12000@gmail.com>
2020-08-29 12:55:16 +0530

    added integer to roman

---
+++ Leetcode problems/roman_to_integr.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+      int return_val(char roman){
+            if (roman == 'I') return 1;
+            if (roman == 'V') return 5;
+            if (roman == 'X') return 10;
+            if (roman == 'L') return 50;
+            if (roman == 'C') return 100;
+            if (roman == 'D') return 500;
+            if (roman == 'M') return 1000;
+      }
+
+public:
+      int roman_to_integer(string s){
+            int result = 0;
+            for (int i=1; i<s.size(); i++){
+                  int num1 = return_val(s[i]);
+                  if(i+1 < s.size()){
+                        int num2 = return_val(s[i+1]);
+                        if(num1 >= num2)
+                              result = num1 + num2;
+                        else
+                              result = result + num2 - num1;
+                  }
+                  else
+                        result = result + num1;
+            }
+            return result;
+      }
+};
+
+int main(){
+      string str;
+      cin >> str;
+      Solution object;
+      cout<< object.roman_to_integer(str);
+}
