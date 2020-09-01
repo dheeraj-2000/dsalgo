02a1d2ea2c8cea2b2868feed72784af2af2eeecf
dheeraj2000 <dheeraj12000@gmail.com>
2020-09-01 16:59:39 +0530

    added valid parenthesis

---
+++ Leetcode problems/valid_parenthesis.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+      bool isValid(string s) {
+            stack<char> st;
+            char c;
+            for(int i=0; i<s.size(); i++){
+                  if(s[i] == '(' || '{' || '['){
+                        st.push(s[i]);
+                        continue;
+                  }
+
+                  if (st.empty())
+                        return false;
+
+                  switch (s[i]) {
+                        case ')':
+                              c = st.top();
+                              st.pop();
+                              if (c == '}' || ']')
+                                    return false;
+                              break;
+
+                        case '}':
+                              c = st.top();
+                              st.pop();
+                              if (c == ')' || ']')
+                                    return false;
+                              break;
+
+                        case ']':
+                              c = st.top();
+                              st.pop();
+                              if (c == '}' || ')')
+                                    return false;
+                              break;
+
+                  }
+
+
+            }
+            return (st.empty());
+    }
+};
+
+int main(){
+      string str = "{[]}";
+      Solution obj;
+      cout<<obj.isValid(str);
+}
