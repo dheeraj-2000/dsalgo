00e9af5a631846c271c01688cf36f7251412806a
dheeraj2000 <dheeraj12000@gmail.com>
2020-09-05 11:20:40 +0530

    added ada matrix

---
+++ contests/codechef/sep20long/ada_matrix.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+      int t;
+      cin>>t;
+      while (t !=0) {
+            /* code */
+            int n;
+            cin>>n;
+            int arr[n][n];
+            for (int i=0; i<n; i++){
+                  for (int j=0; j<n; j++)
+                        cin>> arr[i][j];
+            }
+            for (int i=0; i<n; i++){
+                  for (int j=0; j<n; j++)
+                        cout<< arr[i][j];
+                  cout<<endl;
+            }
+
+            t=t-1;
+      }
+}
