017f2c5ef1487fcc56653e3e5c4c44a4d29c45d5
dheeraj2000 <dheeraj12000@gmail.com>
2020-09-08 21:03:14 +0530

    added chefina_swap

--- contests/codechef/sep20long/chefina_swap.cpp
+++ contests/codechef/sep20long/chefina_swap.cpp
@@ -1,42 +1,26 @@
 #include<bits/stdc++.h>
-#define mod 1000000007
-
 using namespace std;
 
-long long findPos(long long sum) {
- long double ans = sqrtl(1ul+4*(sum));
- ans = ans-1.0;
- ans = ans/2.0;
- long long finalAns = ans;
- return finalAns;
-}
-
-void subMain() {
- long long n;
- cin >> n;        
- long long sum = (n)*(n+1)/2;
- if(sum%2!=0) {
-  cout << 0;
-  return;
- }
- long long x = findPos(sum);
- long long subsum = x*(x+1)/2;
- if(sum/2 == subsum) {
-  long long result = (x*(x-1ul))/2l+((n-x)*(n-x-1ul))/2l+(n-x);
-  cout << result;
- }
- else
-  cout << n-x;
-}
-
 int main() {
- ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
- int t = 1;
- cin >> t;
- while(t--) {
-  subMain();
-  cout << "\n";
- }
- return 0;
+      int t;
+      cin >> t;
+      while(t--) {
+            long long n;
+            cin >> n;
+            long long sum = (n)*(n+1)/2;
+            if(sum%2!=0) {
+                  cout << 0<<endl;
+                  continue;}
+
+            long double temp = sqrtl(1ul+4*(sum));
+            temp = temp-1.0;
+            temp = temp/2.0;
+            long long pos = temp;
+            long long partial_sum = pos*(pos+1)/2;
+
+            if(sum/2 == partial_sum) {
+                  long long result = (pos*(pos-1ul))/2l+((n-pos)*(n-pos-1ul))/2l+(n-pos);
+                  cout <<result<<endl;
+            }else  cout <<n-pos<<endl;
+      }
 }
