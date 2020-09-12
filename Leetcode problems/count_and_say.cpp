43a86e0d8eeaf7a95c0a8aea30aff9539c7c9ac0
dheeraj2000 <dheeraj12000@gmail.com>
2020-09-12 12:55:44 +0530

    added count and say

---
+++ Leetcode problems/count_and_say.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+      string countAndSay(int n) {
+            if(n==1)
+                  return "1";
+  }
+};
+
+
+int main(){
+      int n;
+      cin>>n;
+      Solution dheeraj;
+      cout<<dheeraj.countAndSay(n);
+}
