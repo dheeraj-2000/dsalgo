5e470abfbb210a1d58d508ff789d322d5b60588b
dheeraj2000 <dheeraj12000@gmail.com>
2020-09-14 12:42:52 +0530

    updated plus one for LSB zeros

--- Leetcode problems/plus_one.cpp
+++ Leetcode problems/plus_one.cpp
@@ -4,25 +4,47 @@ using namespace std;
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-      int n = digits.size();
-      int i = n-1;
-      if(digits[i] != 9){
-            digits[i]++;
-      }
-      else{
-            while(digits[i] == 9 && i > 0){
-                  digits[i] = 0;
-                  i--;
-            }
-            if(i == 0 && digits[i] == 9){
-                  digits[i] = 0;
-                  auto it = digits.insert(digits.begin(),1,1);
-            }
-            else{
+
+          // int x=0;
+          // int temp = digits[0];
+          // while(temp==0){
+          //       // int x=0;
+          //       temp = digits[x+1];
+          //       x++;
+          //       // count++;
+          //   }
+            // for(int i=x; i<digits.size(); i++)
+
+            int n = digits.size();
+            int i = n-1;
+            if(digits[i] != 9){
                   digits[i]++;
             }
-      }
-      return digits;
+            else{
+                  while(digits[i] == 9 && i > 0){
+                        digits[i] = 0;
+                        i--;
+                  }
+                  if(i == 0 && digits[i] == 9){
+                        digits[i] = 0;
+                        auto it = digits.insert(digits.begin(),1,1);
+                  }
+                  else{
+                        digits[i]++;
+                  }
+            }
+            int x=0;
+            int temp = digits[0];
+            while(temp==0){
+                  // int x=0;
+                  temp = digits[x+1];
+                  x++;
+                  // count++;
+              }
+             vector<int>::iterator it1, it2;
+            it1 = digits.begin(), it2=digits.begin() + x;
+            digits.erase(it1, it2);
+            return digits;
       }
 };
 
@@ -38,6 +60,15 @@ int main(){
       }
       Solution s;
       s.plusOne(v);
+
+      // int x=0;
+      // int temp = v[0];
+      // while(temp==0){
+      //      // int x=0;
+      //      temp = v[x+1];
+      //      x++;
+      //      // count++;
+      //   }
       for(int i=0; i<v.size(); i++)
             cout<<v[i];
       // cout<<s.plus_one(v)<<endl;
