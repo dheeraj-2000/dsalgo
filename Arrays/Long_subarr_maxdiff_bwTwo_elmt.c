8b444d4a5757d24316d7d5ac1c9252c00ec8eceb
dheeraj2000 <dheeraj12000@gmail.com>
2020-09-15 11:56:28 +0530

    created array folder

---
+++ Arrays/Long_subarr_maxdiff_bwTwo_elmt.c
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+void longest_subarray(int *arr, int n, int diff){
+sort(arr, arr+n);
+      
+
+
+
+}
+
+
+
+
+
+
+
+
+// Driver code
+int main(){
+      int n, diff;
+      cin>>n;
+      int arr[n];
+      for (int i=0; i<n; i++){
+            cin>>arr[i]
+      }
+      cout >> "enter the differenc between two elements of the subarray: ";
+      cin>> diff;
+      longest_subarray(arr, n, diff);
+}
---
+++ Arrays/clockwise_matrix_rotation.cpp
@@ -0,0 +1,58 @@
+// Clockwise rotation of matrix by 90 degree
+// @dheeraj
+
+#include<bits/stdc++.h>
+#define N 3
+using namespace std;
+
+int transpos_matrix(int matrix[N][N]){                   // taking transpose of the matrix
+      for (int i=0; i<N; i++){
+            for (int j=i; j<N; j++){
+                  swap(matrix[i][j], matrix[j][i]);
+            }
+      }
+}
+
+int reverse_rows(int matrix[N][N]){                     // reversing the rows element
+      for (int i=0; i<N; i++){
+            int temp = N-1;
+            for (int j=0; j<temp; j++){
+                  swap(matrix[i][j], matrix[i][temp]);
+                  temp--;
+            }
+      }
+
+}
+
+int print_matrix(int matrix[N][N]){                    // Printing the matrix
+      for (int i=0; i<N; i++){
+            for (int j=0; j<N; j++){
+                  cout << matrix[i][j];
+            }
+            cout << endl;
+      }
+      cout <<endl;
+}
+
+int main(){                                           // Driver function to test above function
+
+      int matrix[N][N] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+      // int N;
+      // cout << "Size of matrix ?: " << endl;
+      // cin>> N;
+      // int matrix[N][N];
+      // cout<<"h";
+      // for (int i=0; i<N; i++){
+      //       for (int j=0; j<N; j++){
+      //             cin>>matrix[i][j];
+      //       }
+      //       cout<<endl;
+      // }
+      cout<< "The given matrix is: " << endl;
+      print_matrix(matrix);
+      transpos_matrix( matrix);
+      reverse_rows( matrix);
+      cout<< "Rotated matrix is: " << "\n" << print_matrix( matrix);
+
+
+}
