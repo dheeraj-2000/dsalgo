fd99153dbf891545f263f6876d719cd2e7edb639
dheeraj2000 <dheeraj12000@gmail.com>
2020-09-18 17:49:27 +0530

    updated find duplicate using hash map

--- Arrays/finding_dublicate.cpp
+++ Arrays/finding_dublicate.cpp
@@ -2,36 +2,48 @@
 #include<vector>
 using namespace std;
 // // Using Brute force Time: (n^2)  Space: (1)
-int solve(vector<int> &A) {
-
-      for (int i=0; i<A.size(); i++){
-            for(int j=i+1;j<A.size();j++){
-                  if(A[j]==A[i])
-                  return A[j];
-                  break;
-            }
-
-      }
-
-      return -1;
-}
+// int solve(vector<int> &A) {
+//
+//       for (int i=0; i<A.size(); i++){
+//             for(int j=i+1;j<A.size();j++){
+//                   if(A[j]==A[i])
+//                   return A[j];
+//                   break;
+//             }
+//
+//       }
+//
+//       return -1;
+// }
 
 // // Using O(n) complexity and O(n) space;
+// int solve(vector<int> &a) {
+//       vector<int> A = a;
+//       for (int i=0; i<A.size(); i++){
+//             int temp = abs(A[i])-1;
+//             if(A[temp] < 0){
+//                   // int x = -A.at(temp);
+//                   return a[i];
+//                   break;
+//             }
+//             A[temp] = -A[temp];
+//       }
+//
+//       return -1;
+// }
+
+
+// Hash map T: O(n), Space: O(n)
 int solve(vector<int> &A) {
-
-      for (int i=0; i<A.size(); i++){
-            int temp = abs(A[i])-1;
-            if(A[temp] < 0){
-                  // int x = -A.at(temp);
-                  return -A[i];
-                  break;
-            }
-            A[temp] = -A[temp];
-      }
-
-      return -1;
+      unordered_map<int,int>counts;
+      for(int i=0;i<A.size();i++)
+            counts[A[i]]++;
+      for(auto it:counts){
+        if(it.second>1)
+            return it.first;
+    }
+    return -1;
 }
-
 
 int main(){
       int n;
