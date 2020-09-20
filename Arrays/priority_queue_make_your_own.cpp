3702fa6ff62808d452864e93e5a24fa175e5e9b7
dheeraj2000 <dheeraj12000@gmail.com>
2020-09-20 15:30:09 +0530

    updated

--- Arrays/priority_queue_make_your_own.cpp
+++ Arrays/priority_queue_make_your_own.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 int main(int argc, char const *argv[]) {
 
-      auto mycomparator = [](int left, int right){return (left) < (right) ; };      // this comparator is used to give priority to the element with your order
+      auto mycomparator = [](int left, int right){return (left) < (right) ; };      // this comparator is used to give priority to the element with your rule
       priority_queue<int, vector<int>, decltype(mycomparator)> dheeraj_pq(mycomparator);
 
       for(int ele: {1,2,4,21,6,8}){
