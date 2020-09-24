c9271b3a8743e716b20e65e0eb0f915840ffdbea
dheeraj2000 <dheeraj12000@gmail.com>
2020-09-24 11:48:40 +0530

    added one more approach for hotel booking

--- Arrays/hotel_bookin_poss.cpp
+++ Arrays/hotel_bookin_poss.cpp
@@ -5,31 +5,52 @@
 
 using namespace std;
 
-bool solve(vector<int> & arrive, vector<int> & depart, int k ){
 
-      vector< pair <int, int> > result;
 
-      for(int i=0; i<arrive.size(); i++){
-            result.push_back(make_pair(arrive[i], 1));
-            result.push_back(make_pair(depart[i], 0));
-      }
-      sort(result.begin(), result.end());
+bool solve(vector<int> &arrive, vector<int> &depart, int K) {
+    sort(arrive.begin(),arrive.end());
+    sort(depart.begin(),depart.end());
+    int i=1,j=0,c=1;
+    while(i<arrive.size()){
+        if(arrive[i]>=depart[j]){
+            i++;
+            j++;
+        }
+        else{
+            i++;
+            c++;
+        }
+    }
+    if(c>K)return false;
 
-      int cur_booked=0, max_booked=0;
-
-      for(int i=0; i<result.size(); i++){
-            if(result[i].second == 1){
-                  cur_booked ++;
-                  max_booked = max(max_booked, cur_booked);
-                  if(max_booked > k)
-                        return false;
-            }
-            else cur_booked++;
-      }
-
-      return true;
-
+    return true;
 }
+
+// bool solve(vector<int> & arrive, vector<int> & depart, int k ){
+//
+//       vector< pair <int, int> > result;
+//
+//       for(int i=0; i<arrive.size(); i++){
+//             result.push_back(make_pair(arrive[i], 1));
+//             result.push_back(make_pair(depart[i], 0));
+//       }
+//       sort(result.begin(), result.end());
+//
+//       int cur_booked=0, max_booked=0;
+//
+//       for(int i=0; i<result.size(); i++){
+//             if(result[i].second == 1){
+//                   cur_booked ++;
+//                   max_booked = max(max_booked, cur_booked);
+//                   if(max_booked > k)
+//                         return false;
+//             }
+//             else cur_booked++;
+//       }
+//
+//       return true;
+//
+// }
 
 
 int main(){
