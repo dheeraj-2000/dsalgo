53c8d65630d306b260feda26c6c6cb23d71e2779
atul goyal <goyalatul278@gmail.com>
2020-10-01 22:37:19 +0530

    string permutation recursive way

---
+++ Leetcode problems/string permutation.java
@@ -0,0 +1,27 @@
+public class ABC {
+    // Function to print all the permutations of str
+    static void printPerm(String str, String ans)
+    {
+        // If string is empty
+        if (str.length() == 0) {
+            System.out.print(ans + " ");
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            // ith character of str
+            char ch = str.charAt(i);
+            // Rest of the string after excluding 
+            // the ith character
+            String ros = str.substring(0, i) + 
+                         str.substring(i + 1);
+            // Recurvise call
+            printPerm(ros, ans + ch);
+        }
+    }
+    // Driver code
+    public static void main(String[] args)
+    {
+        String s = "abb";
+        printPermutn(s, "");
+    }
+}
