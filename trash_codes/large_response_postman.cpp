4a2f5df91e8beb98b6d44053043c7bb6d4dfecd1
Nihar Sanda <55532999+koolgax99@users.noreply.github.com>
2020-10-01 11:13:00 +0530

    added a cpp template and cleaned the code

--- trash_codes/large_response_postman.cpp
+++ trash_codes/large_response_postman.cpp
@@ -6,12 +6,13 @@ using namespace std;
 int main()
 {
     char x;
-    ifstream f("host.txt",ios::ate);
+    ifstream f("host.txt", ios::ate);
     streampos size = f.tellg();
-    for(int var=1;var<=size;var++){
-        f.seekg(-var,ios::end);
+    for (int var = 1; var <= size; var++)
+    {
+        f.seekg(-var, ios::end);
         f.get(x);
-        printf("%c",x);
+        printf("%c", x);
     }
     return 0;
 }
