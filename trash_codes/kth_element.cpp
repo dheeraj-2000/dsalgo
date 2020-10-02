42d5d484eee8af32ddaa9b5f02be617dcece0b17
Nihar Sanda <55532999+koolgax99@users.noreply.github.com>
2020-10-02 09:58:38 +0530

    added a cpp template and formatted the code (#25)
    * added a cpp template and cleaned the code #25 
    
    * codeforces problems #25

--- trash_codes/kth_element.cpp
+++ trash_codes/kth_element.cpp
@@ -1,27 +1,29 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 int search();
-int main(){
+int main()
+{
 	int t;
-	cin>>t;
-	while(t--)
+	cin >> t;
+	while (t--)
 	{
-	    int n,k;
-	    cin>>n;
-	    vector<int>arr;
-	    for(int i=0;i<n;i++)
-	       arr.push_back(search());
-	    cin>>k;
-	    sort(arr.begin(),arr.end());
-	    cout<<arr[k-1]<<endl;
+		int n, k;
+		cin >> n;
+		vector<int> arr;
+		for (int i = 0; i < n; i++)
+			arr.push_back(search());
+		cin >> k;
+		sort(arr.begin(), arr.end());
+		cout << arr[k - 1] << endl;
 	}
 	return 0;
 }
 
-int search(){
-    int ret;
-    cin>>ret;
-    return ret;
+int search()
+{
+	int ret;
+	cin >> ret;
+	return ret;
 }
--- trash_codes/remove_element.py
+++ trash_codes/remove_element.py
@@ -1,5 +1,5 @@
 class Solution:
-    #def removeElement(self, nums: List[int], val: int) -> int:
-      def removeElement(self, nums, val):
-            nums[:] = [x for x in nums if x!=val]
-            return len(nums)   
+    # def removeElement(self, nums: List[int], val: int) -> int:
+    def removeElement(self, nums, val):
+        nums[:] = [x for x in nums if x != val]
+        return len(nums)
