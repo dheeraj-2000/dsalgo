adfcfd13c173d8594d29d0f86380f40cab94cdb6
Ankur Dcruz <42521279+ankurdcruz@users.noreply.github.com>
2020-10-18 18:30:29 +0530

    Update rotateArray.cpp

--- Leetcode problems/rotateArray.cpp
+++ Leetcode problems/rotateArray.cpp
@@ -1,9 +1,13 @@
+//Problem link: https://leetcode.com/problems/rotate-array/
+//Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         
-        int len = nums.size();
-        vector<int> temp(len);
+        int len = nums.size(); //finding length of vector nums
+        vector<int> temp(len); //using an external temp array to store values
         for(int i = 0; i<len; i++)
             temp[(i+k)%len] = nums[i];
         for(int i = 0; i<len; i++)
