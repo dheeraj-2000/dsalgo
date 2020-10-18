54d8eca7e4616b3d3cfdd19b11d9b9ee38c68ad6
unknown <2019346@iiitdmj.ac.in>
2020-10-18 18:19:37 +0530

    Added solution of Div 3-B codeforces program Restore the Permutation by Merger

---
+++ CodeForces Problems/CF-1385-B-Div_3-Restore the Permutation by Merger.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+int main()
+    {
+        long long int t;
+        scanf("%lld", &t);
+        
+        while(t--)
+        {
+            long long int n , i;
+            scanf("%lld", &n);
+            long long int arr[2*n];
+            
+            for(i = 0 ; i < 2*n ; ++i)
+            {
+              scanf("%lld ", &arr[i]);
+            }
+          
+            long long int table[n*2] = {0};
+         
+            for(i = 0 ; i < 2*n ; ++i)
+            {
+          	  //Checking if the value at an index of element number is 0
+              if(table[arr[i]] == 0)
+              {
+              	
+              	//incrementing the value to make it non zero
+               table[arr[i]]++;         
+               printf("%lld ", arr[i]);
+               
+              }
+              
+           }
+         
+            printf("\n");
+    }
+        return 0;
+    }
