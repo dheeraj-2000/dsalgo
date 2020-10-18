ee5a172f15ba67a2a4394092101eb1000673b4d1
Ankur Dcruz <42521279+ankurdcruz@users.noreply.github.com>
2020-10-18 18:33:50 +0530

    Update HouseRobberII.cpp

--- Leetcode problems/HouseRobberII.cpp
+++ Leetcode problems/HouseRobberII.cpp
@@ -1,3 +1,13 @@
+// Problem link: https://leetcode.com/problems/house-robber-ii/
+// You are a professional robber planning to rob houses along a street. 
+// Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. 
+// That means the first house is the neighbor of the last one. 
+// Meanwhile, adjacent houses have a security system connected, and 
+// it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+// Given a list of non-negative integers nums representing the amount of money of each house, 
+// return the maximum amount of money you can rob tonight without alerting the police.
+
 class Solution {
 public:
     int helper(vector<int>& nums, int first, int last) {
