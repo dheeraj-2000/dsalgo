178b261194af6ac92fb61742d14eedf8ad6bfd63
Elismar13 <elismarsilva.5@outlook.com>
2020-10-19 15:56:54 -0300

    Adding linkedList header

---
+++ SimpleLinkedList/linkedList.h
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    struct Node* Next;
+    int v;
+};
+typedef struct Node Node;
+
+typedef struct {
+    Node* begin;
+} List;
+
+// create new List pointer
+List* createList();
+
+// Add information to the list
+void add(List* l, int valor); 
+
+// print all elements in the list
+void printList(List* l);
+
+// check if list is empty
+int isEmpty(List* l);
+
+// remove last element from list
+void removeBack(List* l);
+
+// get the list size
+int size(List* l);
+
