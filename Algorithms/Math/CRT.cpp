038d4f9148f0cd8ab05215f51db5150e9bb2eafd
Elvis Wei <ewei068@berkeley.edu>
2020-10-22 10:32:48 -0500

    Added comments

--- Algorithms/Math/CRT.cpp
+++ Algorithms/Math/CRT.cpp
@@ -2,19 +2,6 @@
 #include <iostream>
 using namespace std;
 
-/* to find (a^b)%m (copied from fermat's)
-Runtime: O(log(b)) */
-int power(int a, unsigned b, unsigned m)
-{
-    if (!b)
-        return 1;
-    int res = power(a, b / 2, m) % m;
-    res = (res * res) % m;
-    if (b % 2 == 0)
-        return res;
-    return (a * res) % m;
-}
-
 /* finds gcd and modular inverse of (a, b) using extended euclid's
 x is the running a^{-1} (mod b) and y is the running b^{-1} (mod a) 
 Runtime: O(log(max(a, b))) */
@@ -50,9 +37,8 @@ int modularInverse(int a, int m)
     }
 }
 
-/* Find the encyrption and decryption of message where
-Public key = (N,e) and private key = d 
-Encryption and decryption runtime: O(log(p*q)) (I think) */
+/* Computes an x that satisfies all equivalencies in equivs[]
+in their respective mod spaces mods [] using Chinese Remainder Theorem */
 void crt(int equivs[], int mods[], int size) {
     int current_total;
     int total_mod = 1;
@@ -69,36 +55,21 @@ void crt(int equivs[], int mods[], int size) {
             }
         }
         int inverse = modularInverse(current_total, mod);
+        if (inverse == -1) {
+            return;
+        }
+
         total_value += inverse * value * current_total;
         total_mod *= mod;
  
     }
-    // cout << total_value;
     cout << total_value % total_mod << endl;
-
-    /*
-    if (p <= 1 || q <= 1 || p == q) {
-        cout << "p and q must be distinct positive primes";
-    }
-
-    int n = p*q;
-    int lambda = (p-1) * (q-1);
-    int d = modularInverse(e, lambda);
-    if (d == -1) {
-        return;
-    }
-    cout << "d: " << d << endl;
-
-    int encryption = power(message, e, n);
-    cout << "Encryption: " << encryption << endl;
-    int decryption = power(encryption, d, n);
-    cout << "Decryption: " << decryption << endl;
-    */
 }
-/* "Input format:" << endl <<
-            "Input 1: number of modular congruncies" << endl <<
-            "Input 2: integer equivalencies a1, a2, ..., am" << endl <<
-            */
+
+/* Computes solution to system of modular congruencies using the Chinese Remainder Theorem
+Input format:
+Input 1: number of modular equivalencies
+Input 2: integer equivalencies a1, a2, ..., am */
 int main()
 {   
     // cout << "Public key = (N, e) where N = pq, p and q are distinct primes, and e is coprime with (p-1)(q-1). Input format: p q e message" << endl;
@@ -111,7 +82,7 @@ int main()
             "finds an integer x (mod n1 * n2 * ... * nm) "
             "such that x = a1 (mod n1); x = a2 (mod n2); ... x = an (mod nm)." << endl;
 
-    cout << "Enter number of conguencies:" << endl;
+    cout << "Enter number of equivalencies:" << endl;
     int size;
     cin >> size;
 
@@ -127,14 +98,6 @@ int main()
     for (int mod_index = 0; mod_index < size; mod_index ++) {
         cin >> mods[mod_index]; 
     }
-
-
-    // int test[] = {2, 3, 2};
-    // int test2[] = {3, 5, 7};
-
-    // int size = sizeof(test)/sizeof(test[0]);
-    // cout << size;
-    // TODO: error checking
 
     crt(equivs, mods, size); 
     return 0;
