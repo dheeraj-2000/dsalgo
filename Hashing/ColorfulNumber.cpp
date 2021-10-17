9f233e7508da2bf0ad28dd6567fbaeb614cb9ea5
jaytenenbaum <37470864+jaytenenbaum@users.noreply.github.com>
2021-10-17 22:05:42 +0300

    Update ColorfulNumber.cpp

--- Hashing/ColorfulNumber.cpp
+++ Hashing/ColorfulNumber.cpp
@@ -2,7 +2,7 @@
 
 /* A number can be broken into different contiguous sub - subsequence parts.Suppose, a number 3245 can be broken into parts like 3 2 4 5 32 24 45 324 245. And this number is a COLORFUL number, since product of every digit of a contiguous subsequence is different int Solution(int num) */
 
-//Cpp soln
+//Cpp solution
 
 int Solution::colorful(int num)
 {
