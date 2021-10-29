ee1974853bbfdfc87c13915429cd1037808af508
sultan arif <53237271+sultanarif-p@users.noreply.github.com>
2021-10-29 15:50:33 +0300

    Create Circular_linkedlist.cpp
    example of circular linkedlist

---
+++ Linked_List/Circular_linkedlist.cpp
@@ -0,0 +1,23 @@
+/* Initialize nodes */
+struct node *head;
+struct node *one = NULL;
+struct node *two = NULL;
+struct node *three = NULL;
+
+/* Allocate memory */
+one = malloc(sizeof(struct node));
+two = malloc(sizeof(struct node));
+three = malloc(sizeof(struct node));
+
+/* Assign data values */
+one->data = 1;
+two->data = 2;
+three->data = 3;
+
+/* Connect nodes */
+one->next = two;
+two->next = three;
+three->next = one;
+
+/* Save address of first node in head */
+head = one;
