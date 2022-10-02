791e132ce96593d6462c21c7929eef3df02ed648
Mohit-Kukreja-2002 <93993902+Mohit-Kukreja-2002@users.noreply.github.com>
2022-10-02 22:16:30 +0530

    Update Sort-0-1-2.cpp
    Added comments :)

--- Arrays/Sort-0-1-2.cpp
+++ Arrays/Sort-0-1-2.cpp
@@ -8,19 +8,32 @@ Output:- [0,0,1,1,1,2,2,2]
 
 #include <bits/stdc++.h> 
 void sort012(int *arr, int n){
+
+    // Using a 3-pointer approach
     int currIndex = 0;
     int left = 0,right = n - 1;
+    
+    
     while (currIndex <= right){
+        // Curr index targets on sorting the numbers where as the Left and Right act as the referencing variables.
+        
         if (arr[currIndex] == 0){
             swap(arr[currIndex], arr[left]);
             currIndex++;
             left++;
         }
+        
+        // arr[currIndex] == 1 ==> now the currindex is at the index between the left and right we just need to increment currIndex
         else if (arr[currIndex] == 1)
             currIndex++;
+            
+        // arr[currIndex]==2 ==> the value should be shifted to the right hand side.
         else{
             swap(arr[currIndex], arr[right]);
             right--;
         }
     }
 }
+
+// The Space Complexity is O(1). as 3 variables are created
+// The Worst Case Time Complexity is O(n) in the case when the array is already sorted.
