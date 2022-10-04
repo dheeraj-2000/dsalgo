a65338f2fd68eea157b7acbc8f0c5dcfc7a68c7b
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2022-10-04 21:28:44 +0530

    feat: :sparkles: add custom sqrt

--- Leetcode problems/sqrt.py
+++ Leetcode problems/sqrt.py
@@ -1,19 +1,23 @@
-def mySqrt(x: int) -> int:
-    if (x < 0): return -1
-    if (x <= 1): return x
+"""
+    !We are using a kind of binary search to implement the solution
+"""
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if (x < 0): return -1
+        if (x <= 1): return x
         
-    lb = 1
-    ub = x // 2
+        lb = 1
+        ub = x // 2
         
-    sqrt = -1
+        sqrt = -1
         
-    while lb <= ub:
-        mid = lb + ((ub - lb) // 2)
+        while lb <= ub:
+            mid = lb + ((ub - lb) // 2)
             
-        if (mid * mid == x): return mid
-        elif (mid * mid > x): ub = mid - 1
-        else:
-            sqrt = mid
-            lb = mid + 1
+            if (mid * mid == x): return mid
+            elif (mid * mid > x): ub = mid - 1
+            else:
+                sqrt = mid
+                lb = mid + 1
         
-    return sqrt
+        return sqrt
