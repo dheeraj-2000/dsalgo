05c9fbfd4e28b994a4c2e25c1655beb4de04d8fb
anuraganand92 <98257282+anuraganand92@users.noreply.github.com>
2022-10-05 15:13:56 +0530

    Catalan numbers using dynamic programming
    Reduces time complexity to O(n^2)

--- Algorithms/Math/CatalanNumbers.cpp
+++ Algorithms/Math/CatalanNumbers.cpp
@@ -3,9 +3,9 @@
 Catalan numbers is a number sequence, which is found useful in a number of
 combinatorial problems, often involving recursively-defined objects.
 */
-
-/* Time Complexity: O(2^n)
-Space Complexity: O(1) */
+//Finding Catalan numbers using dynamic programming
+/* Time Complexity: O(n^2)
+Space Complexity: O(n) */
 
 #include <iostream>
 using namespace std;
@@ -13,12 +13,17 @@ using namespace std;
 // Finding the n_th catalan number
 unsigned long findCatalan(unsigned n)
 {
-    if (n <= 1)
-        return 1;
-    unsigned long result = 0;
-    for (int i = 0; i < n; i++)
-        result = result + findCatalan(i) * findCatalan(n - i - 1);
-    return result;
+    unsigned long catalan[n+1];    //array of catalan numbers
+    catalan[0]=catalan[1]=1;       //initializing first two catalan numbers
+
+    //filling the catalan array using recursion
+    for (int i=2;i<=n;i++) {
+        catalan[i]=0
+        for (int j=0;j<i;j++)
+            catalan[i]+=catalan[j]*catalan[i-j-1];
+    }
+
+    return catalan[n];
 }
 
 int main()
