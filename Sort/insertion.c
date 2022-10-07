162df62a93eecff384e01714636d87a5a6b589a9
Mohit Panthri <99413629+mohitpanthri@users.noreply.github.com>
2022-10-07 22:41:50 +0530

    Update insertion.c
    I added my Insertion sort code so that people can have more choices. Please merge it to the Master Branch.

--- Sort/insertion.c
+++ Sort/insertion.c
@@ -37,3 +37,34 @@ int main() {
 
     return 0;
 }
+
+
+
+// Another Code for Insertion Sort in C
+
+#include <stdio.h>
+int main() {
+  int n, array[100], i, j, temp, flag = 0;
+  printf("Enter how many numbers you want to sort:\n");
+  scanf("%d", &n);
+  printf("Enter %d numbers:\n", n);
+  for (i = 0; i < n; i++)
+    scanf("%d", &array[i]);
+  for (i = 1; i <= n - 1; i++) {
+    temp = array[i];
+    for (j = i - 1; j >= 0; j--) {
+      if (array[j] > temp) {
+        array[j + 1] = array[j];
+        flag = 1;
+      } else
+        break;
+    }
+    if (flag)
+      array[j + 1] = temp;
+  }
+  printf("Sorted list in ascending order:\n");
+  for (i = 0; i <= n - 1; i++) {
+    printf("%d\n", array[i]);
+  }
+  return 0;
+}
