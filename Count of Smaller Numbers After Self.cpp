3de94d7fd0b5b6671dab038236f04ad649e73e69
Bhuwanesh Nainwal <70212380+BhuwaneshNainwal@users.noreply.github.com>
2022-10-09 08:40:47 +0530

    Update Count of Smaller Numbers After Self.cpp

--- Count of Smaller Numbers After Self.cpp
+++ Count of Smaller Numbers After Self.cpp
@@ -1,4 +1,6 @@
 
+
+/*     Approach 1: Using merge-sort technique */
 void merge(vector<pair<int ,int  > > &v,int l,int mid,int r,vector<int > &ans){
     int i=l,j=mid,k=0;
     vector<pair<int ,int > > temp(r-l+1);
@@ -44,5 +46,59 @@ public:
         }
         mergesort(fi,0,x-1,ans);
         return ans;
+    }
+};
+
+
+
+/*     Approach 2: Using Policy Based Data Structure technique */
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <bits/stdc++.h>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+
+class Solution {
+public:
+    
+    void myerase(pbds &t, int v){
+    
+        int rank = t.order_of_key(v);
+        pbds::iterator it = t.find_by_order(rank); 
+        t.erase(it);
+    }
+    vector<int> countSmaller(vector<int>& nums) {
+        
+        int n = nums.size();
+        pbds st;
+        for(int i = n - 1 ; i >= 0 ; i--)
+        {
+            st.insert(nums[i]);
+        }
+        
+        vector<int> count(n);
+        for(int i = 0 ; i < n ; i++)
+        {
+
+            myerase(st, nums[i]);
+            
+            auto it = st.upper_bound(nums[i]);
+            if(it == st.end())
+                count[i] = st.size();
+            
+            else
+                count[i] = st.order_of_key(*it);
+            
+            if(count[i] < 0)
+                count[i] = 0;
+        }
+        
+        return count;
+        
     }
 };
